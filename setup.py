56e12d4d9bc48d651c243b7fa3dee9cce2fa4847
Sam Washington <samwashington@aethris.net>
2015-03-04 23:31:29 +0000

    Added setup.py and setup.cfg

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'requests-aws4auth',
+  packages = ['requests-aws4auth'],
+  version = '0.1',
+  description = 'Amazon Web Services version 4 authentication for the Python requests module'
+  author = 'Sam Washington',
+  author_email = 'samwashington@aethris.net',
+  url = 'https://github.com/sam-washington/requests-aws4auth',
+  download_url = 'https://github.com/samw-washington/requests-aws4auth/tarball/0.1',
+  keywords = ['requests', 'auth', 'authentication', 'amazon', 'amazon web services' 'aws' 's3', 'amazon s3', 'web', 'REST', 'REST API', 'HTTP'],
+  classifiers = [],
+)
