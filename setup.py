d1b9fb4ed1280ffd4aec28a6c7a2a86d9c55184f
Sam <dev@memetree.net>
2015-03-05 00:12:22 +0000

    Fixed typo in setup.py

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from distutils.core import setup
 setup(
   name = 'requests-aws4auth',
   packages = ['requests-aws4auth'],
-  version = '0.1',
+  version = '0.2',
   description = 'Amazon Web Services version 4 authentication for the Python requests module'
   author = 'Sam Washington',
   author_email = 'samwashington@aethris.net',
   url = 'https://github.com/sam-washington/requests-aws4auth',
-  download_url = 'https://github.com/samw-washington/requests-aws4auth/tarball/0.1',
+  download_url = 'https://github.com/sam-washington/requests-aws4auth/tarball/0.2',
   keywords = ['requests', 'auth', 'authentication', 'amazon', 'amazon web services' 'aws' 's3', 'amazon s3', 'web', 'REST', 'REST API', 'HTTP'],
   classifiers = [],
 )
