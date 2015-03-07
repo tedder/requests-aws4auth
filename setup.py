305a11586aece69e063bd0a6cdc5a73b57623426
Sam Washington <codegreen@aethris.net>
2015-03-07 23:29:01 +0000

    Including README, LICENSE, NOTICE and test script in packaged module

--- setup.py
+++ setup.py
@@ -43,6 +43,9 @@ setup(
     license='MIT License',
     keywords='requests authentication amazon web services aws s3 REST',
     install_requires=['requests'],
+    package_data={'requests_aws4auth': ['test/requests_aws4auth_test.py',
+                                        '../README.rst', '../LICENSE',
+                                        '../NOTICE']},
     classifiers=[
         'Development Status :: 3 - Alpha',
         'Intended Audience :: Developers',
